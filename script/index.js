29a04a9aa882b51149b4ea496dbc0d25402de44c
Willyamdevon <dezhintern@gmail.com>
2024-02-09 01:10:36 +0300

    checked

--- script/index.js
+++ script/index.js
@@ -5,3 +5,13 @@ for (let i = 0; i < elList.length; i++) {
     let el = elList[i];
     el.classList.add(`t-alig-c`);
 }
+
+let u = 0;
+
+if (u>=0){
+    pass
+} else if (u==0){
+    pass
+} else{
+    pass
+}
